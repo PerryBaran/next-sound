e638ef402844c98af26315c3e02c3d8d62268670
Perry Baran <perrybaran@gmail.com>
2023-02-20 13:12:24 +0000

    renders albums

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,43 @@
 import { Inter } from '@next/font/google'
+import { getAlbums } from '@/requests/albums'
+import Album from '@/components/album/Album'
 
 const inter = Inter({ subsets: ['latin'] })
 
+export const revalidate = 100
+
+interface Songs {
+  name: string
+  url: string
+  id: string
+}
+
+interface Albums {
+  User: { name: string, id: string }
+  name: string
+  url: string
+  id: string
+  Songs: Songs[]
+}
+
 export default async function Home() {
-  const res = await fetch(`https://soundcloud-clone-api.onrender.com/albums`);
-  const data = await res.json();
+  const albums: Albums[] = await getAlbums(undefined);
 
   return (
     <main>
-      <h1>Home</h1>
+      {albums.map((album) => {
+        return (
+          <Album 
+            key={album.id}
+            artistName={album.User.name}
+            albumUserId={album.User.id}
+            albumName={album.name}
+            albumArt={album.url}
+            songs={album.Songs}
+            albumId={album.id}
+          />
+        )
+      })}
     </main>
   )
 }
