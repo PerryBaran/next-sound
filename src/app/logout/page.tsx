ff63161e2897e160c41410ce2c3d331befdc42a8
Perry Baran <perrybaran@gmail.com>
2023-02-20 11:47:38 +0000

    added logout

--- src/app/logout/page.tsx
+++ src/app/logout/page.tsx
@@ -1,7 +1,23 @@
+'use client'
+
+import { useRouter } from 'next/navigation'
+import { useUserContext } from '@/context/UserContext'
+
 export default function LogOut() {
+  const router = useRouter()
+  const { handleLogin } = useUserContext()
+
+  const handleLogout = () => {
+    handleLogin(undefined)
+    router.push('/')
+  }
+
   return (
     <div>
-      logout
+      <h2>Are You Sure?</h2>
+      <button type='button' onClick={handleLogout}>
+        Logout
+      </button>
     </div>
   )
 }
