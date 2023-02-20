93f3d56d1495c06a9043e78198097ba4afff4657
Perry Baran <perrybaran@gmail.com>
2023-02-20 13:11:48 +0000

    updated config to allow images from aws s3 bucket

--- next.config.js
+++ next.config.js
@@ -3,6 +3,16 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'soundcloud-clone.s3.eu-west-2.amazonaws.com',
+        port: '',
+        pathname: '/**'
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
