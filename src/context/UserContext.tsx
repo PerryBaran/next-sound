c7c860c04f7f35d4c81a0fb561707b5bffa15170
Perry Baran <perrybaran@gmail.com>
2023-02-20 11:21:19 +0000

    added user login from cookie

--- src/context/UserContext.tsx
+++ src/context/UserContext.tsx
@@ -1,6 +1,10 @@
 'use client'
 
-import { createContext, useContext, useState } from 'react'
+import { createContext, useContext, useState, useEffect } from 'react'
+import { getUserById } from '@/requests/users'
+import Cookie from 'js-cookie'
+import jwtDecode from 'jwt-decode'
+
 
 interface User {
   name?: string,
@@ -35,6 +39,19 @@ function UserProvider({
       })
     }
   }
+
+  useEffect(() => {
+    (async () => {
+      const token = Cookie.get("userToken");
+      if (!token) return;
+      const { id }: { id: number | string } = jwtDecode(token);
+      const response = await getUserById(id);
+      handleLogin({
+        name: response.name,
+        id: response.id,
+      });
+    })();
+  }, []);  
 
   return (
     <UserContext.Provider value={{user, handleLogin}}>
