d17667910d0aad1ab3bc44a75880c4c63c66fc38
Perry Baran <perrybaran@gmail.com>
2023-02-20 13:12:08 +0000

    imported icons

---
+++ src/media/icons/index.ts
@@ -0,0 +1,31 @@
+import menu from "./menu.png";
+import music from "./music.png";
+import pause from "./pause.png";
+import play from "./play.png";
+import skip from "./skip.png";
+import volumeLow from "./volume-low.png";
+import volumeMedium from "./volume-medium.png";
+import volumeMute from "./volume-mute.png";
+import home from "./home.png";
+import login from "./login.png";
+import logout from "./logout.png";
+import profile from "./profile.png";
+import search from "./search.png";
+import upload from "./upload.png";
+
+export {
+  menu,
+  music,
+  pause,
+  play,
+  skip,
+  volumeLow,
+  volumeMedium,
+  volumeMute,
+  home,
+  login,
+  logout,
+  profile,
+  search,
+  upload,
+};
