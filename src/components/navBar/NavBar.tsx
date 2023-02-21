c834d13e173cca88218906da7a4d34108f41a3a6
Perry Baran <perrybaran@gmail.com>
2023-02-21 13:01:04 +0000

    addtional condtional styling to navbar

--- src/components/navBar/NavBar.tsx
+++ src/components/navBar/NavBar.tsx
@@ -1,9 +1,31 @@
+'use client'
+
 import Link from 'next/link'
-import SearchBar from './searchBar/SearchBar'
-import CondtionalNav from './conditionalNav/CondtionalNav'
+import { useRouter } from 'next/navigation'
+import { usePathname } from 'next/navigation';
+import { useRef } from 'react'
+import { useUserContext } from '@/context/UserContext'
 import css from './navbar.module.css'
+import { search } from '@/media/icons';
+import Image from 'next/image';
 
 export default function NavBar() {
+  const router = useRouter()
+  const {
+    user: { name }
+  } = useUserContext()
+  const pathname = usePathname()
+  const searchRef = useRef<HTMLInputElement>(null)
+  
+
+  const submitHandler = (e: React.FormEvent) => {
+    e.preventDefault()
+    const query = searchRef?.current?.value
+    if (query) {
+      router.push(`search/${query}`)
+    }
+  }
+
   return (
     <nav className={css.container}>
       <ul>
@@ -12,13 +34,39 @@ export default function NavBar() {
             <h1>Next-Sound</h1>
           </Link>
         </li>
-        <li>
+        <li className={pathname === '/' ? css.active : ''}>
           <Link href="/">Home</Link>
         </li>
         <li>
-          <SearchBar />
+          <form onSubmit={submitHandler} className={css.search}>
+            <input type="text" ref={searchRef} placeholder="Search" />
+            <button type="submit">
+              <Image
+                src={search}
+                alt='search'
+                height={15}
+                width={15}
+              />
+            </button>
+          </form>
         </li>
-        <CondtionalNav />
+        {name ? (
+          <>
+            <li className={pathname === `/profile/${name}` ? css.active : ''}>
+              <Link href={`/profile/${name}`}>Profile</Link>
+            </li>
+            <li className={pathname === '/upload' ? css.active : ''}>
+              <Link href="/upload">Upload</Link>
+            </li>
+            <li className={pathname === '/logout' ? css.active : ''}>
+              <Link href="/logout">Logout</Link>
+            </li>
+          </>
+        ) : (
+          <li className={pathname === '/login' ? css.active : ''}>
+            <Link href="/login">Login</Link>
+          </li>
+        )}
       </ul>
     </nav>
   )
