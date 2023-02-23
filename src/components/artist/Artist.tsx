a33c5df3ac93cd686232631b3534906ceb307a20
Perry Baran <perrybaran@gmail.com>
2023-02-23 21:12:34 +0000

    created Artist card

---
+++ src/components/artist/Artist.tsx
@@ -0,0 +1,28 @@
+import Image from 'next/image'
+import css from './artist.module.css'
+import { profile } from '@/media/icons'
+import Link from 'next/link'
+
+interface Props {
+  name: string
+  image?: string
+}
+
+export default function Artist({
+  name,
+  image
+}: Props) {
+  return (
+    <div className={css["artist"]}>
+      <Link href={`/profile/${name}`} className={css["artist__link"]}>
+        <Image
+          src={image || profile}
+          alt={`album by ${name}`}
+          height={100}
+          width={100}
+        />
+        <h2 className={css["artist__name"]}>{name}</h2>
+      </Link>
+    </div>
+  )
+}
