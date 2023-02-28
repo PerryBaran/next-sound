d5331b220031380fd77a387a06336bb59bff76ca
Perry Baran <perrybaran@gmail.com>
2023-02-28 21:21:45 +0000

    started addin upload album form

--- src/app/upload/page.tsx
+++ src/app/upload/page.tsx
@@ -1,3 +1,65 @@
+'use client'
+
+import Alert from "@/components/alert/Alert"
+import { useRouter } from "next/navigation"
+import { useState } from "react"
+
 export default function Upload() {
-  return <div>upload</div>
+  const [name, setName] = useState("")
+  const [image, setImage] = useState<File | null>(null)
+  const [alert, setAlert] = useState("")
+  const router = useRouter()
+
+  const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setName(e.target.value)
+  }
+
+  const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { files } = e.target;
+    if (!files) return
+  
+    setImage(files[0])
+  }
+
+  const hanldeSubmit = (e: React.FormEvent) => {
+    e.preventDefault()
+
+    if (!name) {
+      setAlert('Must provide a name')
+    } else {
+      const album = {
+        name,
+        image
+      }
+    }
+  }
+
+  return (
+    <div>
+      <Alert message={alert} />
+      <form>
+        <h2>Create Album</h2>
+        <label htmlFor='name'>
+          <span>Name</span>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            value={name}
+            onChange={handleNameChange}
+          />
+        </label>
+        <label htmlFor='image'>
+          <span>Cover Art</span>
+          <input
+            type='file'
+            name='image'
+            id='image'
+            onChange={handleImageChange}
+          />
+        </label>
+        <button type='submit'>Create Album</button>
+      </form>
+    </div>
+  )
 }
