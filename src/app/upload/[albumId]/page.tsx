413424b485a3c5bacd6e8650d5c837ee236490c8
Perry Baran <perrybaran@gmail.com>
2023-03-04 13:00:34 +0000

    started creating UploadSongs

--- src/app/upload/[albumId]/page.tsx
+++ src/app/upload/[albumId]/page.tsx
@@ -1,11 +1,26 @@
 'use client'
 
+import { useState } from "react"
+import Alert from "@/components/alert/Alert"
+
 interface Props {
   params: { albumId: string }
 }
 
+interface Songs {
+  name: string,
+  audio?: File,
+  key: string
+}
+
 export default function UploadSongs(props: Props) {
   const { albumId } = props.params
+  const [songs, setSongs] = useState<Songs[]>([{
+    name: "",
+    audio: undefined,
+    key: crypto.randomUUID()
+  }])
+  const [alert, setAlert] = useState("")
 
   return (
     <div>{albumId}</div>
