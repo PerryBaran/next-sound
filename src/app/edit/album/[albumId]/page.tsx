9fe7de43b4f7200710befe9b99542bba16433e8f
Perry Baran <perrybaran@gmail.com>
2023-03-06 15:49:28 +0000

    added submit and draggable

--- src/app/edit/album/[albumId]/page.tsx
+++ src/app/edit/album/[albumId]/page.tsx
@@ -1,9 +1,13 @@
 "use client"
 
-import { useState, useEffect } from "react"
+import { useState, useEffect, useRef } from "react"
+import { useRouter } from "next/navigation"
 import useSWR from "swr"
 import { getByIdRequestSWR } from "@/requests/helpers"
 import Alert from "@/components/alert/Alert"
+import { patchAlbums, deleteAlbums } from "@/requests/albums"
+import { patchSongs, deleteSongs } from "@/requests/songs"
+import { useUserContext } from "@/context/UserContext"
 
 interface Props {
   params: { albumId: string }
@@ -18,6 +22,7 @@ interface Songs {
   name: string
   audio?: File
   delete: boolean
+  position: number
   id: string
   key: string
 }
@@ -35,13 +40,18 @@ export default function EditAlbum(props: Props) {
     `/albums/${albumId}`,
     getByIdRequestSWR
   )
-
+  const [placeholder, setPlaceholders] = useState(data)
   const [album, setAlbum] = useState<Album>({
     name: "",
     image: undefined
   })
   const [songs, setSongs] = useState<Songs[]>([])
   const [alert, setAlert] = useState("")
+  const dragOver = useRef(0)
+  const router = useRouter()
+  const {
+    user: { name }
+  } = useUserContext()
 
   const handleAlbumNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { value } = e.target
@@ -104,25 +114,88 @@ export default function EditAlbum(props: Props) {
     })
   }
 
-  const handleSubmit = (e: React.FormEvent) => {
+  const dragEnter = (e: React.DragEvent<HTMLDivElement>, position: number) => {
+    dragOver.current = position
+  }
+
+  const dragEnd = (
+    e: React.DragEvent<HTMLDivElement>,
+    startPosition: number
+  ) => {
+    const endPosition = dragOver.current
+
+    const _songs = [...songs]
+    const songContent = _songs[startPosition]
+    _songs.splice(startPosition, 1)
+    _songs.splice(endPosition, 0, songContent)
+    setSongs(_songs)
+
+    const _placeholders = { ...placeholder }
+    const _placeholdersSongs = _placeholders.Songs
+    const _placeholderContent = _placeholdersSongs[startPosition]
+    _placeholdersSongs.splice(startPosition, 1)
+    _placeholdersSongs.splice(endPosition, 0, _placeholderContent)
+    setPlaceholders(_placeholders)
+  }
+
+  const handleSubmit = async (e: React.FormEvent, addSongs = false) => {
     e.preventDefault()
 
-    console.log(album, songs)
+    const promises = []
+
+    if (album.name || album.image) {
+      const data = {
+        name: album.name || undefined,
+        image: album.image || undefined
+      }
+      promises.push(patchAlbums(albumId, data))
+    }
+
+    let position = 0
+    songs.forEach((song) => {
+      if (song.delete) {
+        promises.push(deleteSongs(song.id))
+      } else {
+        if (song.name || song.audio || song.position !== position) {
+          const data = {
+            name: song.name !== "" ? song.name : undefined,
+            audio: song.audio || undefined,
+            position: song.position !== position ? song.position : undefined
+          }
+          promises.push(patchSongs(song.id, data))
+        }
+        position += 1
+      }
+    })
+
+    try {
+      await Promise.all(promises)
+
+      if (addSongs) {
+        router.push(`/upload/${albumId}?albumLength=${position + 1}`)
+      } else {
+        router.push(`profile/${name}`)
+      }
+    } catch (err: any | Error) {
+      setAlert(err?.message || "Unexpected Error")
+    }
   }
 
   useEffect(() => {
     if (data && data.Songs) {
       setSongs(() => {
-        return data.Songs.map((song) => {
+        return data.Songs.map((song, i) => {
           return {
             name: "",
             audio: undefined,
             delete: false,
             id: song.id,
+            position: i,
             key: crypto.randomUUID()
           }
         })
       })
+      setPlaceholders(data)
     }
   }, [data])
 
@@ -138,7 +211,7 @@ export default function EditAlbum(props: Props) {
               type="text"
               id="album-name"
               value={album.name}
-              placeholder={data?.name}
+              placeholder={placeholder?.name}
               onChange={handleAlbumNameChange}
             />
           </label>
@@ -150,7 +223,12 @@ export default function EditAlbum(props: Props) {
         <h3>Edit Songs</h3>
         {songs.map((song, i) => {
           return (
-            <div key={song.key}>
+            <div
+              key={song.key}
+              draggable
+              onDragEnter={(e) => dragEnter(e, i)}
+              onDragEnd={(e) => dragEnd(e, i)}
+            >
               <h4>Song {i + 1}</h4>
               <label htmlFor={`song-name${i}`}>
                 <span>Song Name</span>
@@ -159,7 +237,7 @@ export default function EditAlbum(props: Props) {
                   id={`song-name${i}`}
                   name="name"
                   value={song.name}
-                  placeholder={data?.Songs[i]?.name}
+                  placeholder={placeholder?.Songs[i]?.name}
                   onChange={(e) => handleSongNameChange(e, i)}
                 />
               </label>
@@ -184,7 +262,11 @@ export default function EditAlbum(props: Props) {
             </div>
           )
         })}
-        <button type="submit">Submit</button>
+        <button type="submit">Save Changes</button>
+        <button type="button" onClick={(e) => handleSubmit(e, true)}>
+          Save Changes & Add New Songs
+        </button>
+        <button type="button">Cancel</button>
       </form>
     </div>
   )
