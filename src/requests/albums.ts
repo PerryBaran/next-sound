166e597b8223c3565a27b3dbbbc58a5e942d3f72
Perry Baran <perrybaran@gmail.com>
2023-03-06 15:50:26 +0000

    added patchAlbums and deleteAlbums

--- src/requests/albums.ts
+++ src/requests/albums.ts
@@ -1,4 +1,4 @@
-import { postRequest, getRequest } from "./helpers"
+import { postRequest, getRequest, patchRequest, deleteRequest } from "./helpers"
 
 export async function postAlbums(data: { name: string; image?: File }) {
   const response = await postRequest("albums", data)
@@ -15,5 +15,18 @@ export async function getAlbums(
     | undefined
 ) {
   const response = await getRequest("albums", query)
+  return response
+}
+
+export async function patchAlbums(
+  id: string,
+  data: { name?: string; image?: File }
+) {
+  const response = await patchRequest("albums", id, data)
+  return response
+}
+
+export async function deleteAlbums(id: string) {
+  const response = await deleteRequest("albums", id)
   return response
 }
