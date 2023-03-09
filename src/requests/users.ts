2e823f2c41e16ab285be82b6b4d26d289341f369
Perry Baran <perrybaran@gmail.com>
2023-03-09 12:54:36 +0000

    added and reordered userRequests

--- src/requests/users.ts
+++ src/requests/users.ts
@@ -1,23 +1,11 @@
-import { postRequest, getRequest, getByIdRequest } from "./helpers"
+import {
+  postRequest,
+  getRequest,
+  getByIdRequest,
+  patchRequest,
+  deleteRequest
+} from "./helpers"
 import Cookies from "js-cookie"
-
-export async function getUsers(
-  query:
-    | {
-        name?: string
-        exact?: boolean
-        limit?: number | string
-      }
-    | undefined
-) {
-  const response = await getRequest("users", query)
-  return response
-}
-
-export async function getUserById(id: string | number) {
-  const response = await getByIdRequest("users", id)
-  return response
-}
 
 export async function login(data: { email: string; password: string }) {
   const response = await postRequest("users/login", data)
@@ -32,5 +20,40 @@ export async function signup(data: {
 }) {
   const response = await postRequest("users/signup", data)
   Cookies.set("userToken", response.userToken, { expires: 5 })
+  return response
+}
+
+export async function getUsers(
+  query:
+    | {
+        name?: string
+        exact?: boolean
+        limit?: number | string
+      }
+    | undefined
+) {
+  const response = await getRequest("users", query)
+  return response
+}
+
+export async function getUserById(id: string) {
+  const response = await getByIdRequest("users", id)
+  return response
+}
+
+export async function patchUser(
+  id: string,
+  data: {
+    name?: string
+    email?: string
+    password?: string
+  }
+) {
+  const response = await patchRequest("users", id, data)
+  return response
+}
+
+export async function deleteUser(id: string, password: string) {
+  const response = await deleteRequest("users", `${id}/${password}`)
   return response
 }
