94af2cc79dcef9a77c6604b6821da088690644eb
Perry Baran <perrybaran@gmail.com>
2023-03-21 10:54:22 +0000

    added AlubmSongs tests

---
+++ __tests__/components/AlbumSongs.test.tsx
@@ -0,0 +1,34 @@
+import { render, screen } from "@testing-library/react"
+import AlbumSongs from "../../src/components/album/albumSongs/AlbumSongs"
+import * as SongMenu from "../../src/components/songMenu/SongMenu"
+
+describe("AlbumSongs", () => {
+  const props = {
+    songName: "song-name",
+    audio: "song-audio",
+    image: "album-image",
+    artistName: "artist-name",
+    albumName: "albumName"
+  }
+
+  let mockedSongMenu = jest.fn()
+
+  jest.spyOn(SongMenu, "default").mockImplementation((props: any) => {
+    mockedSongMenu(props)
+    return <div />
+  })
+  
+  test("renders correctly", () => {
+    render(<AlbumSongs {...props}/>)
+
+    expect(screen.getByRole("listitem")).toBeTruthy()
+    expect(screen.getByRole("heading")).toHaveTextContent(props.songName)
+    expect(mockedSongMenu).toHaveBeenCalledWith({songs: [props]})
+  })
+
+  test("snapshot", () => {
+    const { asFragment } = render(<AlbumSongs {...props}/>)
+
+    expect(asFragment()).toMatchSnapshot()
+  })
+})
