275e7632d3e4a882090ee5748ae879d76036eca3
Perry Baran <perrybaran@gmail.com>
2023-03-22 14:40:39 +0000

    added Playlist tests

---
+++ __tests__/components/Playlist.test.tsx
@@ -0,0 +1,101 @@
+import { fireEvent, render, screen } from "@testing-library/react"
+import { createContext, useContext } from "react"
+import Playlist from "../../src/components/musicPlayer/playlist/Playlist"
+import * as PlaylistContext from "../../src/context/PlaylistContext"
+
+describe("Playlist", () => {
+  const mockPlaylist = [
+    {
+      image: "/image-1",
+      artistName: "artist-1",
+      albumName: "album-1",
+      songName: "song-1",
+      audio: "audio-1",
+      key: "key-1"
+    },
+    {
+      artistName: "artist-2",
+      albumName: "album-2",
+      songName: "song-2",
+      audio: "audio-2",
+      key: "key-2"
+    },
+  ]
+  let mockSkipSong = jest.fn()
+  let mockRemoveFromPlaylist = jest.fn()
+  let mockHandlePlaying = jest.fn()
+
+  beforeEach(() => {
+    mockSkipSong = jest.fn()
+    mockRemoveFromPlaylist = jest.fn()
+    mockHandlePlaying = jest.fn()
+
+    const mockContext = createContext({
+      playlist: mockPlaylist,
+      playlistIndex: 0,
+      skipSong: mockSkipSong,
+      handleAddToPlaylist: jest.fn(),
+      removeFromPlaylist: mockRemoveFromPlaylist,
+      playing: false,
+      handlePlaying: mockHandlePlaying,
+      handleShuffle: jest.fn()
+    })
+
+    jest
+      .spyOn(PlaylistContext, "usePlaylistContext")
+      .mockImplementation(() => useContext(mockContext))
+  })
+
+  test("renders correctly", () => {
+    render(<Playlist />)
+
+    expect(screen.getByRole("button")).toBeInTheDocument()
+    expect(screen.getByAltText("playlist")).toBeInTheDocument()
+  })
+
+  test("button click renders playlist", () => {
+    render(<Playlist />)
+    fireEvent.click(screen.getByAltText("playlist"))
+
+    expect(screen.getAllByText(/x/i)).toHaveLength(mockPlaylist.length)
+    expect(screen.getAllByAltText(/play now/i)).toHaveLength(mockPlaylist.length)
+
+    mockPlaylist.forEach((song) => {
+      expect(screen.getByAltText(`${song.albumName} cover art`)).toBeInTheDocument()
+      expect(screen.getByText(song.artistName)).toBeInTheDocument()
+      expect(screen.getByText(song.artistName)).toBeInTheDocument()
+    })
+  })
+
+  test("play now click", () => {
+    render(<Playlist />)
+    fireEvent.click(screen.getByAltText("playlist"))
+
+    mockPlaylist.forEach((song, i) => {
+      fireEvent.click(screen.getAllByAltText("play now")[i])
+
+      expect(mockHandlePlaying).toBeCalledTimes(i + 1)
+      expect(mockHandlePlaying).toHaveBeenLastCalledWith(true)
+      expect(mockSkipSong).toBeCalledTimes(i + 1)
+      expect(mockSkipSong).toHaveBeenLastCalledWith(i)
+    })
+  })
+
+  test("x click", () => {
+    render(<Playlist />)
+    fireEvent.click(screen.getByAltText("playlist"))
+
+    mockPlaylist.forEach((song, i) => {
+      fireEvent.click(screen.getAllByText("x")[i])
+
+      expect(mockRemoveFromPlaylist).toBeCalledTimes(i + 1)
+      expect(mockRemoveFromPlaylist).toHaveBeenLastCalledWith(i)
+    })
+  })
+
+  test("snapshot", () => {
+    const { asFragment } = render(<Playlist />)
+
+    expect(asFragment()).toMatchSnapshot()
+  })
+})
--- src/components/musicPlayer/playlist/Playlist.tsx
+++ src/components/musicPlayer/playlist/Playlist.tsx
@@ -1,11 +1,10 @@
 "use client"
 
 import Image from "next/image"
-import { usePlaylistContext } from "@/context/PlaylistContext"
-import css from "./playlist.module.css"
-import { play, music } from "@/media/icons"
 import { useState } from "react"
-import { menu } from "@/media/icons"
+import { usePlaylistContext } from "../../../context/PlaylistContext"
+import css from "./playlist.module.css"
+import { play, music, menu } from "../../../media/icons"
 
 export default function Playlist() {
   const {
