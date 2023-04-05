bfb9af32fa164ac3eafcf8b2e9e66465351b8941
Perry Baran <perrybaran@gmail.com>
2023-04-05 12:39:06 +0100

    finished Upload tests

--- __tests__/app/Upload.test.tsx
+++ __tests__/app/Upload.test.tsx
@@ -1,4 +1,4 @@
-import { fireEvent, render, screen } from "@testing-library/react"
+import { fireEvent, render, screen, waitFor } from "@testing-library/react"
 import { createContext, useContext } from "react"
 import UploadAlbum from "../../src/app/upload/page"
 import * as navigation from "next/navigation"
@@ -33,10 +33,11 @@ describe("Upload", () => {
     jest
       .spyOn(UserContext, "useUserContext")
       .mockImplementation(() => useContext(mockContext))
+
+    render(<UploadAlbum />)
   })
 
   test("renders correctly", () => {
-    render(<UploadAlbum />)
     const nameInputs = screen.getAllByLabelText(/name/i)
 
     expect(screen.getByText(/create album/i)).toBeInstanceOf(HTMLHeadingElement)
@@ -54,8 +55,6 @@ describe("Upload", () => {
   })
 
   test("remove button", () => {
-    render(<UploadAlbum />)
-
     expect(screen.getAllByLabelText(/name/i)).toHaveLength(2)
 
     fireEvent.click(screen.getByText(/x/i))
@@ -64,13 +63,25 @@ describe("Upload", () => {
   })
 
   test("add button", () => {
-    render(<UploadAlbum />)
-
     expect(screen.getAllByLabelText(/audio/i)).toHaveLength(1)
 
     fireEvent.click(screen.getByText("+"))
 
     expect(screen.getAllByLabelText(/audio/i)).toHaveLength(2)
+  })
+
+  test("drag and drop", () => {
+    fireEvent.click(screen.getByText("+"))
+
+    const name = "name"
+    fireEvent.change(screen.getAllByLabelText(/name/i)[1], {
+      target: { value: name }
+    })
+
+    fireEvent.dragEnter(screen.getAllByLabelText(/name/i)[2])
+    fireEvent.dragEnd(screen.getAllByLabelText(/name/i)[1])
+
+    expect(screen.getAllByLabelText(/name/i)[2]).toHaveValue(name)
   })
 
   describe("submit", () => {
@@ -80,19 +91,16 @@ describe("Upload", () => {
 
     beforeEach(() => {
       postAlbumSpy = jest.spyOn(albumRequests, "postAlbums")
-      postSongsSpy = jest.spyOn(songRequests, "postSongs")
+      postSongsSpy = jest.spyOn(songRequests, "postSongs").mockResolvedValue("")
     })
 
-    xtest("album doesn't contain a name", () => {
-      render(<UploadAlbum />)
-
+    test("album doesn't contain a name", () => {
       fireEvent.click(screen.getByText(/upload album/i))
 
       expect(screen.getByText(/must provide a name/i)).toBeInTheDocument()
     })
 
-    xtest("album contains no songs", () => {
-      render(<UploadAlbum />)
+    test("album contains no songs", () => {
       fireEvent.change(screen.getAllByLabelText(/name/i)[0], {
         target: { value: "album-name" }
       })
@@ -105,8 +113,7 @@ describe("Upload", () => {
       ).toBeInTheDocument()
     })
 
-    xtest("atleast one song doesn't contain a name", () => {
-      render(<UploadAlbum />)
+    test("atleast one song doesn't contain a name", () => {
       fireEvent.change(screen.getAllByLabelText(/name/i)[0], {
         target: { value: "album-name" }
       })
@@ -118,8 +125,7 @@ describe("Upload", () => {
       ).toBeInTheDocument()
     })
 
-    xtest("atleast one song doesn't contain audio", () => {
-      render(<UploadAlbum />)
+    test("atleast one song doesn't contain audio", () => {
       fireEvent.change(screen.getAllByLabelText(/name/i)[0], {
         target: { value: "album-name" }
       })
@@ -134,14 +140,16 @@ describe("Upload", () => {
       ).toBeInTheDocument()
     })
 
-    test("all data is correct", () => {
-      render(<UploadAlbum />)
+    test("all data is correct", async () => {
       postAlbumSpy.mockResolvedValue({ id: mockId })
 
-      const albumName = "album-name"
+      const albumData = {
+        name: "album-name"
+      }
       fireEvent.change(screen.getAllByLabelText(/name/i)[0], {
-        target: { value: albumName }
+        target: { value: albumData.name }
       })
+
       const songData = {
         name: "song-name",
         audio: new File([], "audio"),
@@ -150,12 +158,107 @@ describe("Upload", () => {
       fireEvent.change(screen.getAllByLabelText(/name/i)[1], {
         target: { value: songData.name }
       })
-      fireEvent.change(screen.getByLabelText(/audio/i), { target: { files: [songData.audio] }})
-    
+      fireEvent.change(screen.getByLabelText(/audio/i), {
+        target: { files: [songData.audio] }
+      })
+
       fireEvent.click(screen.getByText(/upload album/i))
-      
-      expect(postAlbumSpy).toBeCalledWith({ name: "album-name" })
-      expect(postSongsSpy).toBeCalledWith({ ...songData, AlbumId: mockId})
+
+      await waitFor(() => {
+        expect(postAlbumSpy).toBeCalledWith(albumData)
+        expect(postSongsSpy).toBeCalledWith({ ...songData, AlbumId: mockId })
+        expect(mockPush).toBeCalledWith(`profile/${mockName}`)
+      })
+    })
+
+    test("accepts optional image", async () => {
+      postAlbumSpy.mockResolvedValue({ id: mockId })
+
+      const albumData = {
+        name: "album-name",
+        image: new File([], "image")
+      }
+      fireEvent.change(screen.getAllByLabelText(/name/i)[0], {
+        target: { value: albumData.name }
+      })
+      fireEvent.change(screen.getByLabelText(/cover art/i), {
+        target: { files: [albumData.image] }
+      })
+
+      const songData = {
+        name: "song-name",
+        audio: new File([], "audio"),
+        position: 0
+      }
+      fireEvent.change(screen.getAllByLabelText(/name/i)[1], {
+        target: { value: songData.name }
+      })
+      fireEvent.change(screen.getByLabelText(/audio/i), {
+        target: { files: [songData.audio] }
+      })
+
+      fireEvent.click(screen.getByText(/upload album/i))
+
+      await waitFor(() => {
+        expect(postAlbumSpy).toBeCalledWith(albumData)
+        expect(postSongsSpy).toBeCalledWith({ ...songData, AlbumId: mockId })
+        expect(mockPush).toBeCalledWith(`profile/${mockName}`)
+      })
+    })
+
+    test("request throws expected error", async () => {
+      const error = "error"
+      postAlbumSpy.mockRejectedValue({ message: error })
+
+      const albumName = "album-name"
+      fireEvent.change(screen.getAllByLabelText(/name/i)[0], {
+        target: { value: albumName }
+      })
+
+      const songData = {
+        name: "song-name",
+        audio: new File([], "audio"),
+        position: 0
+      }
+      fireEvent.change(screen.getAllByLabelText(/name/i)[1], {
+        target: { value: songData.name }
+      })
+      fireEvent.change(screen.getByLabelText(/audio/i), {
+        target: { files: [songData.audio] }
+      })
+
+      fireEvent.click(screen.getByText(/upload album/i))
+
+      await waitFor(() => {
+        expect(screen.getByText(error)).toBeInTheDocument()
+      })
+    })
+
+    test("request throws unexpected error", async () => {
+      postAlbumSpy.mockRejectedValue("")
+
+      const albumName = "album-name"
+      fireEvent.change(screen.getAllByLabelText(/name/i)[0], {
+        target: { value: albumName }
+      })
+
+      const songData = {
+        name: "song-name",
+        audio: new File([], "audio"),
+        position: 0
+      }
+      fireEvent.change(screen.getAllByLabelText(/name/i)[1], {
+        target: { value: songData.name }
+      })
+      fireEvent.change(screen.getByLabelText(/audio/i), {
+        target: { files: [songData.audio] }
+      })
+
+      fireEvent.click(screen.getByText(/upload album/i))
+
+      await waitFor(() => {
+        expect(screen.getByText("Unexpected Error")).toBeInTheDocument()
+      })
     })
   })
 })
