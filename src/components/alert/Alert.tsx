b879341adfea78734e244a3865fa1480a71ecfbf
Perry Baran <perrybaran@gmail.com>
2023-06-08 12:21:13 +0100

    styling alert

--- src/components/alert/Alert.tsx
+++ src/components/alert/Alert.tsx
@@ -1,5 +1,7 @@
+import css from "./alert.module.css"
+
 export default function Alert({ message }: { message: string }) {
   if (!message) return null
 
-  return <p>{message}</p>
+  return <p className={css["alert"]}>{message}</p>
 }
